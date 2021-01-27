6f57afce38c9160f6c1d98922086927e585b1494
felix t <41817193+o2bomb@users.noreply.github.com>
2021-01-27 16:16:04 +0800

    Add working prototype for creating and getting timestamped notes

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -17,6 +17,13 @@ export type Query = {
   __typename?: 'Query';
   hello: Scalars['String'];
   todos: Array<Todo>;
+  getLecture?: Maybe<Lecture>;
+  lectures: Array<Lecture>;
+};
+
+
+export type QueryGetLectureArgs = {
+  id: Scalars['Int'];
 };
 
 export type Todo = {
@@ -27,9 +34,29 @@ export type Todo = {
   updatedAt: Scalars['String'];
 };
 
+export type Lecture = {
+  __typename?: 'Lecture';
+  id: Scalars['Float'];
+  videoUrl: Scalars['String'];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
+  notes: Array<Note>;
+};
+
+export type Note = {
+  __typename?: 'Note';
+  id: Scalars['Float'];
+  content: Scalars['String'];
+  timestamp: Scalars['Float'];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
   addTodo?: Maybe<Todo>;
+  createLecture?: Maybe<Lecture>;
+  createNote?: Maybe<Note>;
 };
 
 
@@ -37,104 +64,232 @@ export type MutationAddTodoArgs = {
   content: Scalars['String'];
 };
 
-export type RegularTodoFragment = (
-  { __typename?: 'Todo' }
-  & Pick<Todo, 'id' | 'content' | 'createdAt' | 'updatedAt'>
+
+export type MutationCreateLectureArgs = {
+  videoUrl: Scalars['String'];
+};
+
+
+export type MutationCreateNoteArgs = {
+  timestamp: Scalars['Int'];
+  content: Scalars['String'];
+  lectureId: Scalars['Int'];
+};
+
+export type RegularLectureFragment = (
+  { __typename?: 'Lecture' }
+  & Pick<Lecture, 'id' | 'videoUrl' | 'createdAt' | 'updatedAt'>
+  & { notes: Array<(
+    { __typename?: 'Note' }
+    & RegularNoteFragment
+  )> }
 );
 
-export type AddTodoMutationVariables = Exact<{
-  content: Scalars['String'];
+export type RegularNoteFragment = (
+  { __typename?: 'Note' }
+  & Pick<Note, 'id' | 'content' | 'timestamp' | 'createdAt' | 'updatedAt'>
+);
+
+export type CreateLectureMutationVariables = Exact<{
+  videoUrl: Scalars['String'];
 }>;
 
 
-export type AddTodoMutation = (
+export type CreateLectureMutation = (
   { __typename?: 'Mutation' }
-  & { addTodo?: Maybe<(
-    { __typename?: 'Todo' }
-    & RegularTodoFragment
+  & { createLecture?: Maybe<(
+    { __typename?: 'Lecture' }
+    & RegularLectureFragment
   )> }
 );
 
-export type GetTodosQueryVariables = Exact<{ [key: string]: never; }>;
+export type CreateNoteMutationVariables = Exact<{
+  lectureId: Scalars['Int'];
+  content: Scalars['String'];
+  timestamp: Scalars['Int'];
+}>;
 
 
-export type GetTodosQuery = (
+export type CreateNoteMutation = (
+  { __typename?: 'Mutation' }
+  & { createNote?: Maybe<(
+    { __typename?: 'Note' }
+    & RegularNoteFragment
+  )> }
+);
+
+export type GetLectureQueryVariables = Exact<{
+  id: Scalars['Int'];
+}>;
+
+
+export type GetLectureQuery = (
   { __typename?: 'Query' }
-  & { todos: Array<(
-    { __typename?: 'Todo' }
-    & RegularTodoFragment
+  & { getLecture?: Maybe<(
+    { __typename?: 'Lecture' }
+    & RegularLectureFragment
   )> }
 );
 
-export const RegularTodoFragmentDoc = gql`
-    fragment RegularTodo on Todo {
+export type GetLecturesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetLecturesQuery = (
+  { __typename?: 'Query' }
+  & { lectures: Array<(
+    { __typename?: 'Lecture' }
+    & RegularLectureFragment
+  )> }
+);
+
+export const RegularNoteFragmentDoc = gql`
+    fragment RegularNote on Note {
   id
   content
+  timestamp
   createdAt
   updatedAt
 }
     `;
-export const AddTodoDocument = gql`
-    mutation AddTodo($content: String!) {
-  addTodo(content: $content) {
-    ...RegularTodo
+export const RegularLectureFragmentDoc = gql`
+    fragment RegularLecture on Lecture {
+  id
+  videoUrl
+  notes {
+    ...RegularNote
+  }
+  createdAt
+  updatedAt
+}
+    ${RegularNoteFragmentDoc}`;
+export const CreateLectureDocument = gql`
+    mutation CreateLecture($videoUrl: String!) {
+  createLecture(videoUrl: $videoUrl) {
+    ...RegularLecture
   }
 }
-    ${RegularTodoFragmentDoc}`;
-export type AddTodoMutationFn = Apollo.MutationFunction<AddTodoMutation, AddTodoMutationVariables>;
+    ${RegularLectureFragmentDoc}`;
+export type CreateLectureMutationFn = Apollo.MutationFunction<CreateLectureMutation, CreateLectureMutationVariables>;
 
 /**
- * __useAddTodoMutation__
+ * __useCreateLectureMutation__
  *
- * To run a mutation, you first call `useAddTodoMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useAddTodoMutation` returns a tuple that includes:
+ * To run a mutation, you first call `useCreateLectureMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateLectureMutation` returns a tuple that includes:
  * - A mutate function that you can call at any time to execute the mutation
  * - An object with fields that represent the current status of the mutation's execution
  *
  * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
  *
  * @example
- * const [addTodoMutation, { data, loading, error }] = useAddTodoMutation({
+ * const [createLectureMutation, { data, loading, error }] = useCreateLectureMutation({
  *   variables: {
- *      content: // value for 'content'
+ *      videoUrl: // value for 'videoUrl'
  *   },
  * });
  */
-export function useAddTodoMutation(baseOptions?: Apollo.MutationHookOptions<AddTodoMutation, AddTodoMutationVariables>) {
-        return Apollo.useMutation<AddTodoMutation, AddTodoMutationVariables>(AddTodoDocument, baseOptions);
+export function useCreateLectureMutation(baseOptions?: Apollo.MutationHookOptions<CreateLectureMutation, CreateLectureMutationVariables>) {
+        return Apollo.useMutation<CreateLectureMutation, CreateLectureMutationVariables>(CreateLectureDocument, baseOptions);
       }
-export type AddTodoMutationHookResult = ReturnType<typeof useAddTodoMutation>;
-export type AddTodoMutationResult = Apollo.MutationResult<AddTodoMutation>;
-export type AddTodoMutationOptions = Apollo.BaseMutationOptions<AddTodoMutation, AddTodoMutationVariables>;
-export const GetTodosDocument = gql`
-    query GetTodos {
-  todos {
-    ...RegularTodo
+export type CreateLectureMutationHookResult = ReturnType<typeof useCreateLectureMutation>;
+export type CreateLectureMutationResult = Apollo.MutationResult<CreateLectureMutation>;
+export type CreateLectureMutationOptions = Apollo.BaseMutationOptions<CreateLectureMutation, CreateLectureMutationVariables>;
+export const CreateNoteDocument = gql`
+    mutation CreateNote($lectureId: Int!, $content: String!, $timestamp: Int!) {
+  createNote(lectureId: $lectureId, content: $content, timestamp: $timestamp) {
+    ...RegularNote
   }
 }
-    ${RegularTodoFragmentDoc}`;
+    ${RegularNoteFragmentDoc}`;
+export type CreateNoteMutationFn = Apollo.MutationFunction<CreateNoteMutation, CreateNoteMutationVariables>;
 
 /**
- * __useGetTodosQuery__
+ * __useCreateNoteMutation__
  *
- * To run a query within a React component, call `useGetTodosQuery` and pass it any options that fit your needs.
- * When your component renders, `useGetTodosQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a mutation, you first call `useCreateNoteMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateNoteMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createNoteMutation, { data, loading, error }] = useCreateNoteMutation({
+ *   variables: {
+ *      lectureId: // value for 'lectureId'
+ *      content: // value for 'content'
+ *      timestamp: // value for 'timestamp'
+ *   },
+ * });
+ */
+export function useCreateNoteMutation(baseOptions?: Apollo.MutationHookOptions<CreateNoteMutation, CreateNoteMutationVariables>) {
+        return Apollo.useMutation<CreateNoteMutation, CreateNoteMutationVariables>(CreateNoteDocument, baseOptions);
+      }
+export type CreateNoteMutationHookResult = ReturnType<typeof useCreateNoteMutation>;
+export type CreateNoteMutationResult = Apollo.MutationResult<CreateNoteMutation>;
+export type CreateNoteMutationOptions = Apollo.BaseMutationOptions<CreateNoteMutation, CreateNoteMutationVariables>;
+export const GetLectureDocument = gql`
+    query GetLecture($id: Int!) {
+  getLecture(id: $id) {
+    ...RegularLecture
+  }
+}
+    ${RegularLectureFragmentDoc}`;
+
+/**
+ * __useGetLectureQuery__
+ *
+ * To run a query within a React component, call `useGetLectureQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetLectureQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = useGetTodosQuery({
+ * const { data, loading, error } = useGetLectureQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useGetLectureQuery(baseOptions: Apollo.QueryHookOptions<GetLectureQuery, GetLectureQueryVariables>) {
+        return Apollo.useQuery<GetLectureQuery, GetLectureQueryVariables>(GetLectureDocument, baseOptions);
+      }
+export function useGetLectureLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetLectureQuery, GetLectureQueryVariables>) {
+          return Apollo.useLazyQuery<GetLectureQuery, GetLectureQueryVariables>(GetLectureDocument, baseOptions);
+        }
+export type GetLectureQueryHookResult = ReturnType<typeof useGetLectureQuery>;
+export type GetLectureLazyQueryHookResult = ReturnType<typeof useGetLectureLazyQuery>;
+export type GetLectureQueryResult = Apollo.QueryResult<GetLectureQuery, GetLectureQueryVariables>;
+export const GetLecturesDocument = gql`
+    query GetLectures {
+  lectures {
+    ...RegularLecture
+  }
+}
+    ${RegularLectureFragmentDoc}`;
+
+/**
+ * __useGetLecturesQuery__
+ *
+ * To run a query within a React component, call `useGetLecturesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetLecturesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetLecturesQuery({
  *   variables: {
  *   },
  * });
  */
-export function useGetTodosQuery(baseOptions?: Apollo.QueryHookOptions<GetTodosQuery, GetTodosQueryVariables>) {
-        return Apollo.useQuery<GetTodosQuery, GetTodosQueryVariables>(GetTodosDocument, baseOptions);
+export function useGetLecturesQuery(baseOptions?: Apollo.QueryHookOptions<GetLecturesQuery, GetLecturesQueryVariables>) {
+        return Apollo.useQuery<GetLecturesQuery, GetLecturesQueryVariables>(GetLecturesDocument, baseOptions);
       }
-export function useGetTodosLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetTodosQuery, GetTodosQueryVariables>) {
-          return Apollo.useLazyQuery<GetTodosQuery, GetTodosQueryVariables>(GetTodosDocument, baseOptions);
+export function useGetLecturesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetLecturesQuery, GetLecturesQueryVariables>) {
+          return Apollo.useLazyQuery<GetLecturesQuery, GetLecturesQueryVariables>(GetLecturesDocument, baseOptions);
         }
-export type GetTodosQueryHookResult = ReturnType<typeof useGetTodosQuery>;
-export type GetTodosLazyQueryHookResult = ReturnType<typeof useGetTodosLazyQuery>;
-export type GetTodosQueryResult = Apollo.QueryResult<GetTodosQuery, GetTodosQueryVariables>;
+export type GetLecturesQueryHookResult = ReturnType<typeof useGetLecturesQuery>;
+export type GetLecturesLazyQueryHookResult = ReturnType<typeof useGetLecturesLazyQuery>;
+export type GetLecturesQueryResult = Apollo.QueryResult<GetLecturesQuery, GetLecturesQueryVariables>;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,77 +1,89 @@
 import Head from "next/head";
-import { useState } from "react";
-import { RegularTodoFragmentDoc, useAddTodoMutation, useGetTodosQuery } from "../generated/graphql";
+import { FormEvent, useState } from "react";
+import YouTube from "react-youtube";
+import {
+  useCreateNoteMutation,
+  useGetLectureQuery,
+} from "../generated/graphql";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const { data, loading, error } = useGetTodosQuery();
-  const [addTodo] = useAddTodoMutation({
-    update(cache, { data: newData }) {
-      const { addTodo } = newData as any;
-      cache.modify({
-        fields: {
-          todos(existingTodos = []) {
-            const newTodoRef = cache.writeFragment({
-              data: addTodo,
-              fragment: RegularTodoFragmentDoc
-            });
-            return [...existingTodos, newTodoRef];
-          }
-        }
-      });
-    }
-  });
   const [value, setValue] = useState("");
+  const [target, setTarget] = useState<any>();
+
+  const { data, loading, error } = useGetLectureQuery({
+    variables: {
+      id: 1,
+    },
+  });
+  const [createNote] = useCreateNoteMutation();
+
+  const setVideoTime = (timestamp: number) => {
+    if (target === undefined || target === null) {
+      return;
+    }
+    target.seekTo(timestamp);
+  };
+
+  const handleSubmitNote = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const currentTimestamp = Math.round(target.getCurrentTime());
+
+    createNote({
+      variables: {
+        lectureId: 1,
+        content: value,
+        timestamp: currentTimestamp,
+      },
+    });
+  };
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>React w/GraphQL server template</title>
+        <title>timestamped-notes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Todo List</h1>
-        <form className={styles.form} onSubmit={(e) => {
-          e.preventDefault();
-          addTodo({
-            variables: {
-              content: value
-            }
-          });
-          setValue("");
-        }}>
-          <input
-            className={styles.input}
-            type="text"
-            placeholder="Get eggs from the green grocer"
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-          />
-        </form>
-        {data?.todos.length === 0 ? (
-          <div>You have no todos. Make some!</div>
-        ) : (
-          <ul className={styles.ul}>
-            {data?.todos.map((todo, index) => {
-              return <li key={index}>{todo.content}</li>;
+        <YouTube
+          videoId="kd1u1ZdJz4w"
+          onReady={(e) => {
+            setTarget(e.target);
+            console.log(e.target);
+          }}
+        />
+        <div className={styles.notes}>
+          <div>
+            {data?.getLecture?.notes.map((note, index) => {
+              return (
+                <div key={index}>
+                  <button
+                    onClick={() => {
+                      setVideoTime(note.timestamp);
+                    }}
+                  >
+                    {note.timestamp}
+                  </button>
+                  {note.content}
+                </div>
+              );
             })}
-          </ul>
-        )}
+          </div>
+          <form onSubmit={handleSubmitNote}>
+            <input
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+              type="text"
+              placeholder="Make notes here"
+            />
+            <button type="submit">Create note</button>
+          </form>
+        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/o2bomb/react-graphql"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Source code available on Github{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
