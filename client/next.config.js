e8898c276f25908b7582a53d7c32ca8d8957a9b3
felix t <41817193+o2bomb@users.noreply.github.com>
2021-02-04 16:11:19 +0800

    Setup proxy from client to server for dev environment

---
+++ client/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async rewrites() {
+    return process.env.NODE_ENV === "development" ? [
+      {
+        source: '/auth/github',
+        destination: 'http://server:5000/auth/github',
+      },
+    ] : []
+  },
+}
