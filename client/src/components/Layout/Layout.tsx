80ef90d23318aff4b42a399630e04d92a5c8c8e9
felix t <41817193+o2bomb@users.noreply.github.com>
2021-02-05 01:05:43 +0800

    Add Space Mono google font

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -13,6 +13,11 @@ const Layout: React.FC = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Navbar />
       <main>{children}</main>
