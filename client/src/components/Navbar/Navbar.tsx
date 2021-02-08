9acc11f5b4b9fc0bcb2dcb9382a1c4701fb9711e
felix t <41817193+o2bomb@users.noreply.github.com>
2021-02-08 18:38:36 +0800

    Make navbar and home, login page more mobile friendly

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,29 +1,40 @@
 import React from "react";
 import Link from "next/link";
-import { useMeQuery } from "../../generated/graphql";
+import { useGetLecturesQuery, useMeQuery } from "../../generated/graphql";
+import { useViewport } from "../../utils/ViewportProvider";
 
 import styles from "./Navbar.module.css";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
+  const viewport = useViewport();
   const { data, loading, error } = useMeQuery();
+  const { data: lectureData } = useGetLecturesQuery();
+
+  const buttons = () => {
+    return data?.me ? (
+      <>
+        {(lectureData?.lectures || []).length > 0 ? (
+          <Link href="/create-lecture">
+            <button>Create a lecture</button>
+          </Link>
+        ) : null}
+        <Link href="/logout">Logout</Link>
+      </>
+    ) : (
+      <Link href="/login">Sign in</Link>
+    );
+  };
 
   return (
     <header className={styles.navbar}>
       <div className={styles.brand}>
-        <Link href="/">timestamped_notes</Link>
+        <Link href="/">
+          {viewport.width > 580 ? "timestamped_notes" : "t_n"}
+        </Link>
       </div>
-      <div className={styles.buttonGroup}>
-        {data?.me ? (
-          <>
-            <Link href="/create-lecture">Create a lecture</Link>
-            <Link href="/logout">Logout</Link>
-          </>
-        ) : (
-          <Link href="/login">Sign in</Link>
-        )}
-      </div>
+      <div className={styles.buttonGroup}>{buttons()}</div>
     </header>
   );
 };
